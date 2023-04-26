70c1e4dd23af37576265bd23e61e472f3b077861
Guillermo Santos <guillermo.santosba@gmail.com>
2023-04-26 15:46:44 -0600

    Finished algorithm

--- programacion_dq.py
+++ programacion_dq.py
@@ -6,9 +6,37 @@ def edit_distance(string1, string2):
 		if m == 0 or n == 0:
 				return max(m, n)	# Si alguna de las dos cadenas está vacía, la distancia es la longitud de la otra cadena
 		
+		# Divide
 		if string1[-1] == string2[-1]:
 				return edit_distance(string1[:-1], string2[:-1])
 		else:
 				return 1 + min(edit_distance(string1[:-1], string2), edit_distance(string1, string2[:-1]), edit_distance(string1[:-1], string2[:-1]))
 
-print(edit_distance("hello", "yellow"))
+
+contenido = []
+
+
+with open("listado_entradas.txt", "r") as archivo:
+    contenido = archivo.read()
+    contenido = contenido.split('\n')
+
+times = []
+for line in contenido:
+    words = line.split(',')
+    t = timeit.timeit(stmt=lambda: edit_distance(words[0], words[1]), number=1)
+    times.append(t)
+
+etiquetas = list(range(len(times)))
+
+# Crear un gráfico de barras
+plt.bar(etiquetas, times)
+
+# Configurar los límites del eje y
+plt.ylim([0, max(times) * 1.1])
+
+# Agregar etiquetas de texto para los valores de las barras
+for i, valor in enumerate(times):
+    plt.text(i, valor * 1.05, str(valor), ha='center')
+
+# Mostrar el gráfico
+plt.show()
